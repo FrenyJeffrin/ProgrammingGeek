cdff34c21a1fa43915de39195b732d7da240c1f6
Freny Jeffrin Sweety <freny1999@gmail.com>
2023-07-03 19:48:20 +0530

    sorting list with points x and y

--- listWithPointsXY.py
+++ listWithPointsXY.py
@@ -1,3 +1,4 @@
+"""
 class Point:
     def __init__(self,x,y):
         self.x=x
@@ -6,5 +7,17 @@ def myFun(p):
     return p.x
 l=[Point(1,15),Point(10,5),Point(1,8)]
 l.sort(key=myFun)
+for i in l:
+    print(i.x, i.y)
+"""
+#if you want to preserve the order
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def __lessthan__(self,other):
+        return self.x < other.x
+l=[Point(1,15),Point(10,5),Point(1,8)]
+l.sort()
 for i in l:
     print(i.x, i.y)
